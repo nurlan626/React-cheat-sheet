cf5f908ce29988532be3d8301686777fb03ac831
nurlan007 <nurlan@artifactlabs.io>
2022-06-14 23:43:53 +0400

    fix

--- src/components/reactUlbiTv/ReactUlbiTv.jsx
+++ src/components/reactUlbiTv/ReactUlbiTv.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import "../../styles/UlbiTv.css";
+import MyInput from "../UI/input/MyInput";
+import MySelect from "../UI/select/MySelect";
 import PostForm from "./PostForm";
 import PostList from "./PostList";
 
 const ReactUlbiTv = () => {
   const [posts, setPosts] = useState([
-    { id: 1, title: "Java Script", body: "Description" },
-    { id: 2, title: "Java Script 2", body: "Description" },
-    { id: 3, title: "Java Script", body: "Description" },
-    { id: 4, title: "Java Script", body: "Description" },
+    { id: 1, title: "3Java Script", body: "2Description" },
+    { id: 2, title: "2Java Script 2", body: "3Description" },
+    { id: 3, title: "3Java Script", body: "4Description" },
+    { id: 4, title: "4ava Script", body: "1Description" },
   ]);
+  const [selectedSort, setSelectedSort] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+
 
   const createPost = (newPost) => {
     setPosts([...posts, { id: Date.now(), ...newPost }]);
@@ -17,13 +23,31 @@ const ReactUlbiTv = () => {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    console.log(selectedSort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
   return (
     <div>
       <PostForm createPost={createPost} />
+      <MyInput  
+      value={searchQuery}
+      onChange={e => setSearchQuery(e.targer.value)}
+      placeholder="Поиск" />
+      <MySelect
+        onChange={sortPosts}
+        value={selectedSort}
+        options={[
+          { name: "по названию", value: "title" },
+          { name: "по описанию", value: "body" },
+        ]}
+        defaultValue="сортировка по"
+      />
       {posts.length > 0 ? (
         <PostList removePost={removePost} posts={posts} />
       ) : (
-        <div>Net</div>
+        <div>Постов нет</div>
       )}
     </div>
   );
