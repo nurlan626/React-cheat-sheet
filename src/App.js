3520f29dd121cdaa7cd23ccbbd60099f3b6d9bb1
nurlan007 <nurlan@artifactlabs.io>
2022-06-16 22:22:16 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
-import Users from "./components/Users";
-import Features from "./components/Features"
-import ClassCounter from "./components/class/ClassCounter";
+// import { useState } from "react";
+// import Users from "./components/Users";
+// import Features from "./components/Features"
+// import ClassCounter from "./components/class/ClassCounter";
 import ReactUlbiTv from "./components/reactUlbiTv/ReactUlbiTv";
 
 function App() {
-  const [data, setData] = useState([
-    { name: "Nurlan", city: "Baku", age: 29 },
-    { name: "Alex", city: "Odessa", age: 19 },
-  ]);
+  // const [data, setData] = useState([
+  //   { name: "Nurlan", city: "Baku", age: 29 },
+  //   { name: "Alex", city: "Odessa", age: 19 },
+  // ]);
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +19,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
         >
           Learn React
         </a>
