ebb4b5398e1e4a6b0b5a9bfa47b30ade79a1a783
nurlan007 <nurlan@artifactlabs.io>
2022-06-20 00:24:42 +0400

    fi routes

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import "./Navbar.css";
 
 const Navbar = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.logout();
+    navigate("/");
+  };
   return (
     <nav className="navigation">
       <NavLink to="/" className="Link">
@@ -16,14 +22,26 @@ const Navbar = () => {
       <NavLink to="allPosts" className="Link">
         All Posts
       </NavLink>
-      <NavLink to="profile" className="Link">
-        Profile
-      </NavLink>
+      {auth.user && (
+        <NavLink to="profile" className="Link">
+          Profile
+        </NavLink>
+      )}
+
       {!auth.user && (
         <NavLink to="login" className="Link">
           Login
         </NavLink>
       )}
+      <div to="#" className="user">
+        {auth.user && (
+          <div>
+            {auth.user} <button onClick={handleLogout}>Logout</button>
+          </div>
+        )}
+      </div>
+      <span></span>
+      {console.log(auth)}
     </nav>
   );
 };
